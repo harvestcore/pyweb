ae40f4bb18c9b89bbb70c4f4a93d5c42d67ff94c
Ángel Gómez <aagomezies@gmail.com>
2018-10-27 18:40:11 +0200

    db rest

--- p4/db.py
+++ p4/db.py
@@ -8,19 +8,28 @@ dbmongo = client.practica4                  ## Base de datos
 restaurantes = dbmongo.practica4            ## Colección
 
 
-class dbmongo:
+class DBRestaurantes:
     def loadData(url):
         urljson = urllib.request.urlopen(url)
         for row in urljson:
             jrow = loads(row)
             restaurantes.insert_one(jrow)
 
-    def get(namexd):
-        document = restaurantes.find_one({"name": namexd})
+    def getByID(_id):
+        document = restaurantes.find_one({"_id": _id})
+        return document
+
+    def getByName(name):
+        document = restaurantes.find_one({"name": name})
         return document
 
     def getAll():
         return restaurantes.find({})
 
+    def getIntervalo(min, max):
+        datalist = []
+        data = DBRestaurantes.getAll()
+        for i in range(min, max):
+            datalist.append({'_id':str(data[i]['_id']), 'location':data[i]['location'], 'name':data[i]['name']})
 
 
