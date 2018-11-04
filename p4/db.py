579cf304a6a5e336861bdca0006a6a1e90f8d65d
Ángel Gómez <aagomezies@gmail.com>
2018-11-04 12:07:56 +0100

    fin db p4

--- p4/db.py
+++ p4/db.py
@@ -4,11 +4,55 @@ from bson.json_util import loads
 from bson.objectid import ObjectId
 
 client = MongoClient('localhost', 27017)    ## Conexión
-dbmongo = client.practica4                  ## Base de datos
-restaurantes = dbmongo.practica4            ## Colección
+db = client.test                            ## Base de datos
+restaurantes = db.restaurantes              ## Colección
 
 
-class DBRestaurantes:
+class Restaurantes:
+    def loadData(url):
+        urljson = urllib.request.urlopen(url)
+        for row in urljson:
+            jrow = loads(row)
+            restaurantes.insert_one(jrow)
+
+    def getByID(_id):
+        return restaurantes.find_one({"_id": ObjectId(_id)})
+
+    def getByName(name):
+        return restaurantes.find_one({"name": name})
+
+    def getAll():
+        return restaurantes.find({})
+
+    def findAndUpdate(_id, update):
+        return restaurantes.find_one_and_update({'_id': ObjectId(_id)}, {'$set': update}, projection={'seq':True, '_id':False}, upsert = True)
+
+    def size():
+        return restaurantes.count()
+
+    def modifyName(newname):
+        return None
+
+    def add(name, coordinates, _type):
+        return restaurantes.insert_one({'name': name, 'location':{'coordinates': coordinates, 'type': _type}})
+    
+    def delByName(name):
+        return restaurantes.delete_one({'name': name})
+
+    def delByID(_id):
+        return restaurantes.delete_one({'_id': ObjectId(_id)})
+
+    def getIntervalo(min, max):
+        datalist = []
+        data = Restaurantes.getAll()
+        if max > data.count():
+            max = data.count()
+        for i in range(min, max):
+            datalist.append({'_id':str(data[i]['_id']), 'location':data[i]['location'], 'name':data[i]['name']})
+        return datalist
+
+
+class Barrios:
     def loadData(url):
         urljson = urllib.request.urlopen(url)
         for row in urljson:
@@ -37,5 +81,3 @@ class DBRestaurantes:
 
     def size():
         return restaurantes.count()
-
-
